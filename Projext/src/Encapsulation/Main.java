dd5166aeeb9d54075c946bc98f74e5b46fcf283d
ThuyetTVHE186110 <144113388+ThuyetTVHE186110@users.noreply.github.com>
2023-09-07 15:39:15 +0700

    Add files via upload

---
+++ Projext/src/Encapsulation/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Encapsulation;
+
+/**
+ *
+ * @author Asus
+ */
+public class Main {
+    public static void main(String[] args){
+        Car car = new Car();
+        car.setName("Honda");
+        System.out.println(car.getName());
+    }
+}
---
+++ Projext/src/Inheritance/Main.java
@@ -0,0 +1,9 @@
+
+package Inheritance;
+
+public class Main {
+    public static void main(String args[]){
+        Honda honda = new Honda("6",100,true);
+        System.out.println(honda.toString());
+    }
+}
